a0d22cf1b500768e2ab889953c6d9033c2fcea68
José de Senna <josespnt1990@gmail.com>
2026-02-28 14:04:29 -0300

    feat: cria estrututa do programa principal

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Projeto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            Console.WriteLine("Lista de exemplos aplicados:");
+            Console.WriteLine();
+            Console.WriteLine("    Sem exemplos aplicados");
+
+            Console.WriteLine();
+            Console.Write("Digite o número do exemplo que deseja executar ou 0 para sair: ");
+            string? escolha = Console.ReadLine();
+            Console.WriteLine();
+
+            if (escolha == "0")
+            {
+                return;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
